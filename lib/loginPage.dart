24393d651dbcdd737d49396b3b5a56bf03458b4f
Nubla07 <afifamuntaha07@gmail.com>
2023-08-25 17:11:09 +0600

    Day 4 completed

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -6,16 +6,50 @@ class LogInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.amber,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assests/images/undraw_secure_login_pdn4.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: " Enter User Name", labelText: "User Name"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: " Enter Password", labelText: "Password"),
+                  ),
+                  ElevatedButton(
+                    child: Text("Log In"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("Welcome");
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Pages/homepage.dart';
 import 'package:flutter_application_1/loginPage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.cyan),
+        theme: ThemeData(
+          primarySwatch: Colors.cyan,
+          fontFamily: GoogleFonts.lato().fontFamily,
+        ),
         themeMode: ThemeMode.light,
         darkTheme: ThemeData(
           brightness: Brightness.dark,
